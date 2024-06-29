b39df4b73c73750ec82c53fc870573a63b5a1a12
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-29 18:05:02 +0100

    Fixed code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,10 +2,26 @@
 
 file { '/etc/ssh/ssh_config':
   ensure  => present
-  content => "
-  # Configuration of client
-  host *
-  PasswordAuthentication no
-  IdentityFile ~/.ssh/school
-  "
 }
+
+file_line { 'host':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  match  => '^host*',
+  line   => 'host*'
+}
+
+file_line { 'password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  match  => '^ PasswordAuthentication',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'identity_file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  match  => '^IdentityFile',
+  line   => 'IdentityFile ~/.ssh/school',
+}
+  
