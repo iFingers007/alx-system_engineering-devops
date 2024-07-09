6b0d6f023a7dcd11ac418cfbd3b66f80a57b8083
Abu Jehoiada <abujehoiada@gmail.com>
2024-07-09 13:18:33 +0100

    Fixed code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,15 +5,18 @@ package { 'nginx':
 }
 
 exec { 'install':
-    command  => 'sudo apt-get update -y; sudo apt-get -y nginx',
-    provider => shell,
+  command  => 'sudo apt-get update -y; sudo apt-get -y nginx',
+  provider => shell,
 }
 
-file_line { 'Add_Header':
-  path  => '/etc/nginx/sites-enabled/default',
-  match => 'location / {',
-  line  => 'location / {\n\tadd_header X-Served-By \"${hostname}\";',
-}
+exec { 'Add_Header':
+  command => 'sudo sed -i "27i \\\tadd_header X-Served-By ${HOSTNAME};" /etc/nginx/sites-available/default'
+  
+# file_line { 'Add_Header':
+#  path  => '/etc/nginx/sites-enabled/default',
+#  match => 'location / {',
+#  line  => 'location / {\n\tadd_header X-Served-By \"${hostname}\";',
+#}
 
 exec { 'start':
   command  => 'sudo service nginx restart',
