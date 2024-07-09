c57d8dc41b1f124d92b1c02176c29bd4dad4d940
Abu Jehoiada <abujehoiada@gmail.com>
2024-07-09 12:44:56 +0100

    Fixed code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,28 +4,10 @@ package { 'nginx':
   ensure => present,
   }
 
-  exec { 'install_nginx':
-    command  => 'sudo apt-get update -y; sudo apt-get install -y nginx',
-    provider => shell
-#    require  => Package['nginx'],
-  }
-
-  exec { 'Hello':
-    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-    provider => shell,
-#    require  => Exec['install_nginx'],
-  }
-
-  exec { 'configure redirect':
-    command => 'sudo sed -i "s/server_name _;/server_name _;\n\n\tlocation \/redirect_me {\n\t\t return 301 https:\/\/www.google.com;\n\t}" \
-  /etc/nginx/sites-enabled/default',
-    provider => shell,
-#    require  => Exec['install_nginx'],
-  }
-
-  exec { 'Add_Header':
-    command  => 'sudo sed -i "27i \\\tadd_header X-Served-By ${HOSTNAME}" /etc/nginx/sites-available/default',
-    provider => shell,
+  file_line { 'Add_Header':
+    path  => '/etc/nginx/sites-available/default',
+    match => 'location /',
+    line  => 'location /\n\tadd_header X-Served-By \"${hostname}\";',
     }
 
   exec { 'start':
