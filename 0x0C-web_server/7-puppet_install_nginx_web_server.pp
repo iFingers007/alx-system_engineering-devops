108b5bbde5214aebd9a1b993e4ae83ff470cd71a
Abu Jehoiada <abujehoiada@gmail.com>
2024-07-09 12:56:08 +0100

    Fixed code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,25 +2,25 @@
 
 package { 'nginx':
   ensure => present,
-  }
+}
 
-  exec { 'install':
-    command  => 'sudo apt-get update -y; sudo apt-get -y nginx',
-    provider => shell
-  }
+exec { 'install':
+  command  => 'sudo apt-get update -y; sudo apt-get -y nginx',
+  provider => shell
+}
 
-  exec { 'Hello':
-    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-    provider => shell,
-  }
+exec { 'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
 
-  exec { 'configure redirect':
-    command => 'sudo sed -i "s/server_name _;/server_name _;\n\n\tlocation \/redirect_me {\n\t\t return 301 https:\/\/www.google.com;\n\t}" \
-    /etc/nginx/sites-enabled/default',
-    provider => shell,
-  }
+file_line { 'configure redirect':
+  path  => "/etc/nginx/sites-enabled/default",
+  match => "server_name _;",
+  line  => "server_name _;\n\n\tlocation \/redirect_me {\n\t\t return 301 https:\/\/www.google.com;\n\t}",
+}
 
-  exec { 'start':
-    command  => 'sudo service nginx restart',
-    provider =>shell,
-  }
+exec { 'start':
+  command  => 'sudo service nginx restart',
+  provider =>shell,
+}
