37914c7747adbf6364ce386a96768cccddbd4d7a
Abu Jehoiada <abujehoiada@gmail.com>
2024-08-14 09:28:21 +0100

    Fixed code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,23 +15,23 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
+    response = requests.get(url, headers=headers)
 
-    r = requests.get(url, headers=headers).json()
-    subscribers = r.get('data', {}).get('subscribers')
-    if not subscribers:
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            return data['data']['subscribers']
+        except KeyError:
+            return 0
+    else:
         return 0
-    return subscribers
+
+#    r = requests.get(url, headers=headers).json()
+#    subscribers = r.get('data', {}).get('subscribers')
+#    if not subscribers:
+#        return 0
+#    return subscribers
 
 #    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
 #    headers = {'User-Agent': 'subreddit-subscriber-checker/0.1'}
-#
-#    response = requests.get(url, headers=headers)
-#    if response.status_code == 200:
-#        try:
-#            data = response.json()
-#            return data['data']['subscribers']
-#        except KeyError:
-#            return 0
-#    else:
-#        return 0
 #
