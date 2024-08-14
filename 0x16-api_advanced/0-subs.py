0fcdcc7dfb1d1322ddf3e57bcc5c4ad23258a4cf
Abu Jehoiada <abujehoiada@gmail.com>
2024-08-14 09:47:24 +0100

    Fixed code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,19 +12,34 @@ def number_of_subscribers(subreddit):
 
     """
 
-    url = f"https://www.reddit.com/r/{subreddit}/top.json"
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-    response = requests.get(url, headers=headers)
+    headers.update({'User-Agent': 'subreddit-subscriber-checker/0.1'})
+#    headers = {'User-Agent': 'subreddit-subscriber-checker/0.1'}
 
-    if response.status_code == 200:
-        try:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
             data = response.json()
-            return data['data']['subscribers']
-        except KeyError:
+            return data['data'].get('subscribers', 0)
+        else:
             return 0
-    else:
+    except requests.RequestException:
         return 0
+
+#    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+#    user_agent = requests.utils.default_headers()
+#    user_agent.update({'User-Agent': 'My User Agent 1.0'})
+#    response = requests.get(url, headers=user_agent)
+
+#    if response.status_code == 200:
+#        try:
+#            data = response.json()
+#            return data['data']['subscribers']
+#        except KeyError:
+#            return 0
+#    else:
+#        return 0
 
 #    r = requests.get(url, headers=headers).json()
 #    subscribers = r.get('data', {}).get('subscribers')
